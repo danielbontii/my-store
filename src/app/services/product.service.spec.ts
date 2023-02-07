3dd4d8ea98b1c522cbcb1e5c8c6606c6b21aa69b
danielbontii <bontiidaniel97@gmail.com>
2023-02-07 17:21:17 +0000

    wrote product service tests

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 
 import { ProductService } from './product.service';
 
@@ -8,12 +9,29 @@ describe('ProductService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
     });
     service = TestBed.inject(ProductService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return an observable array of products', () => {
+    expect(service.getProducts()).toBeInstanceOf(Observable);
+  });
+
+  it('getProduct() should return selected product', (done) => {
+    service.getProduct(1).subscribe((product) => {
+      expect(product).toEqual({
+        id: 1,
+        name: 'Book',
+        price: 9.99,
+        url: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+        description: 'You can read it!',
+      });
+      done();
+    });
   });
 });
